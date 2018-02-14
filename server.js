ac68d3fc09960c05113550b0d0b3b94796aa1ddd
julie hawkins <julabi@gmail.com>
2018-02-14 11:16:40 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// const environment = process.env.NODE_ENV || 'development';
+// const config = require('./knexfile')[environment];
+// const database = require('knex')(config);
+
+const app = express();

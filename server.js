b092b23f0b862e9f04e9cb5fe438c217396cc389
Thomas Laird <t6r6l5@gmail.com>
2018-02-14 15:07:05 -0700

    basic setup and test endpoint

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
-
-// const environment = process.env.NODE_ENV || 'development';
-// const config = require('./knexfile')[environment];
-// const database = require('knex')(config);
 
 const app = express();
+
+const requireHTTPS = (request, response, next) => {
+  if (request.headers['x-forwarded-proto'] !== 'https') {
+    return response.redirect('https://' + request.get('host') + request.url);
+  }
+  next();
+};
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(requireHTTPS);
+}
+
+app.set('port', process.env.PORT || 9500);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const environment = process.env.NODE_ENV || 'development';
+const config = require('./knexfile')[environment];
+const database = require('knex')(config);
+
+app.listen(app.get('port'));
+
+app.use('/', express.static(`${__dirname}/client/build`));
+
+app.get('/test', (req, res) => {
+  return res.status(200).json({sdjl:'qweqwe'})
+})

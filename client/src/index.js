ef5fce34a2a3cdfa5ff07d1c687fd548915de21e
Thomas Laird <t6r6l5@gmail.com>
2018-02-14 19:20:21 -0700

    add redux related fns to app root

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './Reducers';
+import logger from 'redux-logger';
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(rootReducer, devTools, applyMiddleware(thunk, logger));
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();

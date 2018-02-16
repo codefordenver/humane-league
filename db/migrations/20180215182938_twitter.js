e6a27048156826a0ada345ed9c5a47b06985c661
katiescruggs <katie.e.scruggs@gmail.com>
2018-02-15 18:48:02 -0700

    Create schema for twitter_actions table

--- db/migrations/20180215182938_twitter.js
+++ db/migrations/20180215182938_twitter.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('twitter_actions', (table) => {
+    table.increments('id').primary();
+    table.boolean('enabled');
+    table.string('title');
+    table.text('description', 'longtext');
+    table.string('target');
+    table.timestamps(true, true);
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('twitter_actions');
 };

8400a2cd89543d02d73ef9013e9b0e8aa4c26d78
julie hawkins <julabi@gmail.com>
2018-02-16 18:11:51 -0700

    styles form/input and layout when shown

--- client/src/Components/Welcome/Welcome.js
+++ client/src/Components/Welcome/Welcome.js
@@ -24,16 +24,17 @@ class Welcome extends Component {
 
     const accountForm = 
       <form className='create-account-form'>
-        <input type='text' placeholder='user-name' />
-        <button>Create Account</button>
+        <input className='create-account-user-input' type='text' placeholder='user-name' />
+        <button className='create-account-submit' >Create Account</button>
       </form>;
 
     const imgClassName = !this.state.showForm ? 'fan-logo' : 'fan-logo shifted';
-    
+    const welcomeClassName = !this.state.showForm ? 'Welcome' : 'Welcome shifted';
+
     const controls = !this.state.showForm ? signInPrompt : accountForm;
 
     return (
-      <div className='Welcome'>
+      <div className={welcomeClassName}>
         <img className={imgClassName} src={fanLogo} alt='' />
         {controls}
       </div>

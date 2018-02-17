45291994f139c4cf66b753cb1a8cafc169a7720a
Thomas Laird <t6r6l5@gmail.com>
2018-02-16 21:54:35 -0700

    mock fetch for app test

--- client/src/Components/App/App.test.js
+++ client/src/Components/App/App.test.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+window.fetch = jest.fn().mockImplementation( () => Promise.resolve({
+  json: () => Promise.resolve({
+    status: 'Success'
+  })
+}));
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);

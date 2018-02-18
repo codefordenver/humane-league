333ebba2b34904a6a21ec51195835b79edcb9a04
Thomas Laird <t6r6l5@gmail.com>
2018-02-18 15:15:51 -0700

    add property validation for get responses

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -8,6 +8,16 @@ const environment = process.env.NODE_ENV || 'development';
 const config = require('../knexfile.js')[environment];
 const knex = require('knex')(config);
 
+const props = {
+  action:       ['id', 'user_id', 'action_id', 'action_type', 'description', 'created_at', 'updated_at'],
+  socialAction: ['id', 'enabled', 'title', 'description', 'target', 'created_at', 'updated_at'],
+  contents:     ['id', 'action_id', 'content'],
+  phoneAction:  ['id', 'enabled', 'title', 'description', 'name', 'position', 'phone_number', 'created_at', 'updated_at'],
+  emailAction:  ['id', 'enabled', 'title', 'description', 'to', 'cc', 'bcc', 'subject', 'created_at', 'updated_at'],
+  user:         ['id', 'twitter_actions', 'facebook_actions', 'email_actions', 'phone_actions', 'admin', 'name', 'id_token']
+};
+
+
 chai.use(chaiHttp);
 
 describe('Server routes', () => {
@@ -19,16 +29,16 @@ describe('Server routes', () => {
   });
   describe('get all items routes', () => {
     const routes = [
-      { table: 'action_log',        route: '/api/v1/actions',           length: 0}, 
-      { table: 'twitter_actions',   route: '/api/v1/twitter_actions',   length: 1}, 
-      { table: 'twitter_contents',  route: '/api/v1/twitter_contents',  length: 3}, 
-      { table: 'facebook_actions',  route: '/api/v1/facebook_actions',  length: 1},
-      { table: 'facebook_contents', route: '/api/v1/facebook_contents', length: 3},
-      { table: 'phone_actions',     route: '/api/v1/phone_actions',     length: 1}, 
-      { table: 'phone_contents',    route: '/api/v1/phone_contents',    length: 3}, 
-      { table: 'email_actions',     route: '/api/v1/email_actions',     length: 1}, 
-      { table: 'email_contents',    route: '/api/v1/email_contents',    length: 3}, 
-      { table: 'users',             route: '/api/v1/users',             length: 3}
+      { table: 'action_log',        route: '/api/v1/actions',           expectedProps: props.action,       length: 1}, 
+      { table: 'twitter_actions',   route: '/api/v1/twitter_actions',   expectedProps: props.socialAction, length: 1}, 
+      { table: 'twitter_contents',  route: '/api/v1/twitter_contents',  expectedProps: props.contents,     length: 3}, 
+      { table: 'facebook_actions',  route: '/api/v1/facebook_actions',  expectedProps: props.socialAction, length: 1},
+      { table: 'facebook_contents', route: '/api/v1/facebook_contents', expectedProps: props.contents,     length: 3},
+      { table: 'phone_actions',     route: '/api/v1/phone_actions',     expectedProps: props.phoneAction,  length: 1}, 
+      { table: 'phone_contents',    route: '/api/v1/phone_contents',    expectedProps: props.contents,     length: 3}, 
+      { table: 'email_actions',     route: '/api/v1/email_actions',     expectedProps: props.emailAction,  length: 1}, 
+      { table: 'email_contents',    route: '/api/v1/email_contents',    expectedProps: props.contents,     length: 3}, 
+      { table: 'users',             route: '/api/v1/users',             expectedProps: props.user,         length: 3}
     ];
 
     routes.map( route => {
@@ -40,6 +50,11 @@ describe('Server routes', () => {
             response.should.be.json;
             response.body.results.should.be.a('array');
             response.body.results.length.should.equal(route.length);
+            response.body.results.forEach( (result, index) => {
+              route.expectedProps.forEach( prop => {
+                result.should.have.property(prop);
+              });
+            });
           });
       });
     });
@@ -47,16 +62,16 @@ describe('Server routes', () => {
 
   describe('get one item routes', () => {
     const routes = [
-      { table: 'action_log',        route: '/api/v1/actions/1',           length: 0}, 
-      { table: 'twitter_actions',   route: '/api/v1/twitter_actions/1',   length: 1}, 
-      { table: 'twitter_contents',  route: '/api/v1/twitter_contents/1',  length: 3}, 
-      { table: 'facebook_actions',  route: '/api/v1/facebook_actions/1',  length: 1},
-      { table: 'facebook_contents', route: '/api/v1/facebook_contents/1', length: 3},
-      { table: 'phone_actions',     route: '/api/v1/phone_actions/1',     length: 1}, 
-      { table: 'phone_contents',    route: '/api/v1/phone_contents/1',    length: 3}, 
-      { table: 'email_actions',     route: '/api/v1/email_actions/1',     length: 1}, 
-      { table: 'email_contents',    route: '/api/v1/email_contents/1',    length: 3}, 
-      { table: 'users',             route: '/api/v1/users/1',             length: 3}
+      { table: 'action_log',        route: '/api/v1/actions/1',           expectedProps: props.action         }, 
+      { table: 'twitter_actions',   route: '/api/v1/twitter_actions/1',   expectedProps: props.socialAction         }, 
+      { table: 'twitter_contents',  route: '/api/v1/twitter_contents/1',  expectedProps: props.contents         },
+      { table: 'facebook_actions',  route: '/api/v1/facebook_actions/1',  expectedProps: props.socialAction         },
+      { table: 'facebook_contents', route: '/api/v1/facebook_contents/1', expectedProps: props.contents         },
+      { table: 'phone_actions',     route: '/api/v1/phone_actions/1',     expectedProps: props.phoneAction         }, 
+      { table: 'phone_contents',    route: '/api/v1/phone_contents/1',    expectedProps: props.contents         }, 
+      { table: 'email_actions',     route: '/api/v1/email_actions/1',     expectedProps: props.emailAction         }, 
+      { table: 'email_contents',    route: '/api/v1/email_contents/1',    expectedProps: props.contents         }, 
+      { table: 'users',             route: '/api/v1/users/1',             expectedProps: props.user         }
     ];
 
     routes.map( route => {
@@ -66,6 +81,9 @@ describe('Server routes', () => {
           .then(response => {
             response.should.have.status(200);
             response.should.be.json;
+            route.expectedProps.forEach( prop => {
+              response.body.result.should.have.property(prop);
+            });
           });
       });
     });

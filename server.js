7ed2aec55c9100e0b188a53005ac2344e7694590
Thomas Laird <t6r6l5@gmail.com>
2018-02-17 17:05:43 -0700

    map over array of routes to dynamically create get/post/patch routes

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const routes = require('./serverRoutes/');
+const routes = require('./serverHelper/serverHelpers');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -30,10 +30,54 @@ app.listen(app.get('port'), () => {
 
 app.use('/', express.static(`${__dirname}/client/build`));
 
-app.get('/test', (req, res) => {
-  return res.status(200).json({status:'Successfully hit server endpoint!'})
+const params = {
+  content:     ['action_id', 'content'],
+  log:         ['user_id', 'action_type', 'action_id', 'description'],
+  socialMedia: ['enabled', 'title', 'description', 'target'],
+  phone:       ['enabled', 'title', 'description', 'name', 'phone_number'],
+  email:       ['enabled', 'title', 'description', 'subject', 'to'],
+  users:       ['facebook_actions', 'twitter_actions', 'email_actions', 'phone_actions', 'admin', 'name']
+};
+
+const endpoints = [
+  { table: 'action_log',        all: '/api/v1/actions',           one: '/api/v1/actions/:id',           reqParams: params.log         }, 
+  { table: 'twitter_actions',   all: '/api/v1/twitter_actions',   one: '/api/v1/twitter_actions/:id',   reqParams: params.socialMedia }, 
+  { table: 'twitter_contents',  all: '/api/v1/twitter_contents',  one: '/api/v1/twitter_contents/:id',  reqParams: params.content     }, 
+  { table: 'facebook_actions',  all: '/api/v1/facebook_actions',  one: '/api/v1/facebook_actions/:id',  reqParams: params.socialMedia },
+  { table: 'facebook_contents', all: '/api/v1/facebook_contents', one: '/api/v1/facebook_contents/:id', reqParams: params.content     },
+  { table: 'phone_actions',     all: '/api/v1/phone_actions',     one: '/api/v1/phone_actions/:id',     reqParams: params.phone       }, 
+  { table: 'phone_contents',    all: '/api/v1/phone_contents',    one: '/api/v1/phone_contents/:id',    reqParams: params.content     }, 
+  { table: 'email_actions',     all: '/api/v1/email_actions',     one: '/api/v1/email_actions/:id',     reqParams: params.email       }, 
+  { table: 'email_contents',    all: '/api/v1/email_contents',    one: '/api/v1/email_contents/:id',    reqParams: params.content     }, 
+  { table: 'users',             all: '/api/v1/users',             one: '/api/v1/users/:id',             reqParams: params.users       }
+];
+
+
+endpoints.map( endpoint => {
+  const {table, all } = endpoint;
+  return app.get(all, (req, res) => {
+    return routes.getAllHelper(req, res, database, table);
+  });
 });
 
-app.get('/phonetest', (request, response) => {
-  return routes.phoneRoutes.getPhoneActions(request, response, database);
+endpoints.map( endpoint => {
+  const {table, one } = endpoint;
+  return app.get(one, (req, res) => {
+    return routes.getOneHelper(req, res, database, table);
+  });
 });
+
+endpoints.map( endpoint => {
+  const {table, all, reqParams} = endpoint;
+  return app.post(all, (req, res) => {
+    return routes.postHelper(req, res, database, table, reqParams);
+  });
+});
+
+endpoints.map( endpoint => {
+  const {table, one} = endpoint;
+  return app.patch(one, (req, res) => {
+    return routes.patchHelper(req, res, database, table);
+  });
+});
+

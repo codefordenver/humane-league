a1566d93add31c280800c0aa4f71b2c1e0a2e84f
Thomas Laird <t6r6l5@gmail.com>
2018-02-19 13:44:45 -0700

    add user reducer

--- client/src/Reducers/index.js
+++ client/src/Reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import Placeholder from './Placeholder';
+import User from './User';
 
 
 const rootReducer = combineReducers({
-  Placeholder
+  Placeholder,
+  User
 });
 
 export default rootReducer;

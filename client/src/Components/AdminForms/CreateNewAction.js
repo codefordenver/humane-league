653dc6460036163ad6fc06fcc5f08a4dc079f663
julie hawkins <julabi@gmail.com>
2018-02-20 12:37:12 -0700

    select sets state for type of action form

--- client/src/Components/AdminForms/CreateNewAction.js
+++ client/src/Components/AdminForms/CreateNewAction.js
@@ -1,12 +1,42 @@
-import React from 'react';
-// import './CreateNewAction.css';
+import React, { Component } from 'react';
+import './AdminForms.css';
 
-const CreateNewAction = () => {
-  
+class CreateNewAction extends Component {
+  constructor() {
+    super();
+    this.state = {
+      form: 'facebook'
+    }
+  }
 
-  return (
-    <h1>I'm A new action form</h1>
-  )
+  handleChange = () => {
+    const form = this.refs.actionTypes.value;
+
+    this.setState({ form })
+  }
+
+  render() {
+    const form = 
+      <form>
+        <input type='text' placeholder={this.state.form}/>
+      </form>;
+
+    return (
+      <div className='create-new-action'>
+        <h1>CREATE A NEW ACTION</h1>
+        <label htmlFor='action-types'>Select Action Type:</label>
+        <select onChange={() => this.handleChange()} ref='actionTypes' name='action-types' id='action-types'>
+          <option value='facebook'>Facebook</option>
+          <option value='twitter'>Twitter</option>
+          <option value='email'>Email</option>
+          <option value='phone'>Phone</option>
+        </select>
+        <div className='create-action-form'>
+        {form}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default CreateNewAction;

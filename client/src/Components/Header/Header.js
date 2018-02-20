fb7df10eabd215803c0fb0353500e79fd1da09ef
katiescruggs <katie.e.scruggs@gmail.com>
2018-02-19 23:12:46 -0700

    Add nav links for Settings and Logout

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -4,16 +4,20 @@ import thlLogoWhite from '../../THL-Assets/png/THL18-horiz-logo-white.png';
 const Header = () => {
   return (
     <header>
-          <a 
-            className='main-site'
-            target='_blank' 
-            rel='noopener noreferrer' 
-            href='https://thehumaneleague.org/'>
-            <img 
-              className="App__logo" 
-              src={thlLogoWhite} />
-          </a>
-          <p>Welcome, Katie</p>
+      <a 
+        className='main-site'
+        target='_blank' 
+        rel='noopener noreferrer' 
+        href='https://thehumaneleague.org/'>
+        <img 
+          className="App__logo" 
+          src={thlLogoWhite} />
+      </a>
+      <nav>
+        <p className="welcome">Welcome, Katie</p>
+        <p className="settings-btn nav-btn">Settings</p>
+        <p className="login-logout-btn nav-btn">Logout</p>
+      </nav>
     </header>
   );
 }

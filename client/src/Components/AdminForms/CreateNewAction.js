22915b638e0be93fd58f0419f51e4336b98afa11
julie hawkins <julabi@gmail.com>
2018-02-20 16:02:01 -0700

    adds enable/disable toggle to form/state

--- client/src/Components/AdminForms/CreateNewAction.js
+++ client/src/Components/AdminForms/CreateNewAction.js
@@ -5,7 +5,8 @@ class CreateNewAction extends Component {
   constructor() {
     super();
     this.state = {
-      form: 'facebook'
+      form: 'facebook',
+      enabled: true
     }
   }
 
@@ -40,34 +41,51 @@ class CreateNewAction extends Component {
     return (
       <div className='create-new-action-container'>
         <h1>CREATE A NEW ACTION</h1>
-        <label htmlFor='action-types'>Select Action Type:</label>
-        <select onChange={() => this.handleChange()} ref='actionTypes' name='action-types' id='action-types'>
-          <option value='facebook'>Facebook</option>
-          <option value='twitter'>Twitter</option>
-          <option value='email'>Email</option>
-          <option value='phone'>Phone</option>
-        </select>
-        <div className='create-action-form'>
-          <h2>{this.state.form.toUpperCase()}</h2>
-          <form action='create-new-action-form'>
-            <input type='text' placeholder='Action Title' />
-            <input type='text' placeholder='Action Description' />
-            {form[this.state.form].targetUrl}
+        <section className='select-action-container'>
+          <label htmlFor='action-types'>Select Action Type:
+            <select onChange={() => this.handleChange()} ref='actionTypes' name='action-types' id='action-types'>
+              <option value='facebook'>Facebook</option>
+              <option value='twitter'>Twitter</option>
+              <option value='email'>Email</option>
+              <option value='phone'>Phone</option>
+            </select>
+          </label>
+          <section className='create-action-form'>
+            <h2>{this.state.form.toUpperCase()}</h2>
+            <form action='create-new-action-form'>
+              <input type='text' placeholder='Action Title' />
+              <input type='text' placeholder='Action Description' />
+              {form[this.state.form].targetUrl}
 
-            {form[this.state.form].to}
-            {form[this.state.form].cc}
-            {form[this.state.form].bcc}
-            {form[this.state.form].subject}
+              {form[this.state.form].to}
+              {form[this.state.form].cc}
+              {form[this.state.form].bcc}
+              {form[this.state.form].subject}
 
-            {form[this.state.form].name}
-            {form[this.state.form].position}
-            {form[this.state.form].phoneNumber}
+              {form[this.state.form].name}
+              {form[this.state.form].position}
+              {form[this.state.form].phoneNumber}
 
-            <div>enabledToggle</div>
-            <input type='text' placeholder='Action Content' />
-            <button>CREATE THIS ACTION</button>
-          </form>
-        </div>
+              <div className='enable-toggle'>
+                <span 
+                  id='toggle'>
+                  <input
+                    ref={input => this.checkbox = input} 
+                    type='checkbox' 
+                    checked={this.state.enabled}
+                    onChange={() => this.setState({ enabled: !this.state.enabled })}
+                  />
+                  <label 
+                    data-on='enabled' 
+                    data-off='disabled'>
+                  </label>
+                </span>
+              </div>    
+              <input type='text' placeholder='Action Content' />
+              <button>CREATE THIS ACTION</button>
+            </form>
+          </section>
+        </section>
       </div>
     )
   }

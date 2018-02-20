266eb6283de7af34f80b9baa8e398592821806c9
julie hawkins <julabi@gmail.com>
2018-02-20 13:13:22 -0700

    creates all needed action creation inputs, condition rendering

--- client/src/Components/AdminForms/CreateNewAction.js
+++ client/src/Components/AdminForms/CreateNewAction.js
@@ -12,17 +12,35 @@ class CreateNewAction extends Component {
   handleChange = () => {
     const form = this.refs.actionTypes.value;
 
-    this.setState({ form })
+    this.setState({ form });
   }
 
   render() {
-    const form = 
-      <form>
-        <input type='text' placeholder={this.state.form}/>
-      </form>;
 
+    const socialMediaTarget = {
+      targetUrl: <input type='text' placeholder='Target Url' />
+    }  
+    const emailTarget = {
+      to: <input type='text' placeholder='Target Email Address' />,
+      cc: <input type='text' placeholder='CCs' />,
+      bcc: <input type='text' placeholder='BCCs' />,
+      subject: <input type='text' placeholder='Email Subject' />,
+    }
+    const phoneTarget = {
+      name: <input type='text' placeholder='Target Name' />,
+      position: <input type='text' placeholder='Target Position' />,
+      phoneNumber: <input type='text' placeholder='Target Phone Number' />
+    }
+    const form = {
+      facebook: socialMediaTarget,
+      twitter: socialMediaTarget,
+      email: emailTarget,
+      phone: phoneTarget
+    };
+
+            {form[this.state.form]}
     return (
-      <div className='create-new-action'>
+      <div className='create-new-action-container'>
         <h1>CREATE A NEW ACTION</h1>
         <label htmlFor='action-types'>Select Action Type:</label>
         <select onChange={() => this.handleChange()} ref='actionTypes' name='action-types' id='action-types'>
@@ -32,7 +50,24 @@ class CreateNewAction extends Component {
           <option value='phone'>Phone</option>
         </select>
         <div className='create-action-form'>
-        {form}
+          <h2>{this.state.form.toUpperCase()}</h2>
+          <form action='create-new-action-form'>
+            <input type='text' placeholder='Action Title' />
+            <input type='text' placeholder='Action Description' />
+            {form[this.state.form].targetUrl}
+
+            {form[this.state.form].to}
+            {form[this.state.form].cc}
+            {form[this.state.form].bcc}
+            {form[this.state.form].subject}
+
+            {form[this.state.form].name}
+            {form[this.state.form].position}
+            {form[this.state.form].phoneNumber}
+
+            <div>enabledToggle</div>
+            <input type='text' placeholder='Action Content' />
+          </form>
         </div>
       </div>
     )

19363e74c0110da5e61a70789f91f0903edcbabd
katiescruggs <katie.e.scruggs@gmail.com>
2018-02-21 15:25:21 -0700

    Add checkbox and patch functionality

--- client/src/Components/UserProfile/UserProfile.js
+++ client/src/Components/UserProfile/UserProfile.js
@@ -1,50 +1,74 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './UserProfile.css';
 import { connect } from 'react-redux';
 
-const UserProfile = ({ user }) => {
-  const { name, twitter_actions, facebook_actions, email_actions, phone_actions } = user;
+class UserProfile extends Component {
+  constructor () {
+    super();
+    this.state = {
+      id: 0,
+      name: '',
+      twitter_actions: null,
+      facebook_actions: null,
+      email_actions: null,
+      phone_actions: null
+    };
+  }
 
-  const twitter = twitter_actions ? <input className="checkbox" type="checkbox" value="twitter" checked /> : <input className="checkbox" type="checkbox" value="twitter" />;
-  const facebook = facebook_actions ? <input className="checkbox" type="checkbox" value="facebook" checked /> : <input className="checkbox" type="checkbox" value="facebook"/>;
-  const email = email_actions ? <input className="checkbox" type="checkbox" value="email" checked /> : <input className="checkbox" type="checkbox" value="email"/>;
-  const phone = phone_actions ? <input className="checkbox" type="checkbox" value="phone" checked /> : <input className="checkbox" type="checkbox" value="phone"/>;
+  componentDidMount () {
+    const { id, name, twitter_actions, facebook_actions, email_actions, phone_actions } = this.props.user;
+    this.setState({ id, name, twitter_actions, facebook_actions, email_actions, phone_actions });
+  }
 
-  const patchPreferences = () => {
-    // will get values of checks and make a patch request
+  changeClick = (event) => {
+    this.setState({ [event.target.value]: !this.state[event.target.value] });
+  }
+
+  patchPreferences = async (event) => {
+    event.preventDefault();
+    const { twitter_actions, facebook_actions, email_actions, phone_actions } = this.state;
+    const preferencePath = await fetch(`/api/v1/users/${this.state.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ twitter_actions, facebook_actions, email_actions, phone_actions })
+    });
   };
 
-  return (
-    <div className="UserProfile">
-      <h1>{`Profile for ${name}`}</h1>
-      
-      <div className="user-achievements">
-        <h2>User Achievements</h2>
-      </div>
-      
-      <div className="user-preferences">
-        <h2>Action Preferences</h2>
+  render () {
+    return (
+      <div className="UserProfile">
+        <h1>{`Profile for ${this.state.name}`}</h1>
+        
+        <div className="user-achievements">
+          <h2>User Achievements</h2>
+        </div>
+        
+        <div className="user-preferences">
+          <h2>Action Preferences</h2>
 
-        <form className="user-preferences-form">
-          <label> Twitter Actions
-            {twitter}
-          </label>
-          <label> Facebook Actions
-            {facebook}
-          </label>
-          <label> Email Actions
-            {email}
-          </label>
-          <label> Phone Actions
-            {phone}
-          </label>
-          
-          <button onClick={patchPreferences}>SAVE</button>
-        </form>
+          <form className="user-preferences-form">
+            <label> Twitter Actions
+              <input className="checkbox" type="checkbox" value="twitter_actions" checked={this.state.twitter_actions} onChange={this.changeClick}/>
+            </label>
+            <label> Facebook Actions
+              <input className="checkbox" type="checkbox" value="facebook_actions" checked={this.state.facebook_actions} onChange={this.changeClick}/>
+            </label>
+            <label> Email Actions
+              <input className="checkbox" type="checkbox" value="email_actions" checked={this.state.email_actions} onChange={this.changeClick}/>
+            </label>
+            <label> Phone Actions
+              <input className="checkbox" type="checkbox" value="phone_actions" checked={this.state.phone_actions} onChange={this.changeClick}/>
+            </label>
+            
+            <button onClick={this.patchPreferences}>SAVE</button>
+          </form>
 
+        </div>
       </div>
-    </div>
-  )
+    );
+  }
 }
 
 const mapStateToProps = store => ({

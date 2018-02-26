0aa2af1d2b7c9f59b4890fd1787d38472e55dd3c
julie hawkins <julabi@gmail.com>
2018-02-26 16:56:52 -0700

    creates enable/disable form and fetches all actions

--- client/src/Components/AdminForms/UpdateAction.js
+++ client/src/Components/AdminForms/UpdateAction.js
@@ -1,12 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import './UpdateAction.css';
 
-const UpdateAction = () => {
-  
+class UpdateAction extends Component {
+  constructor() {
+    super();
+    this.state = {
+      actionType: 'facebook',
+      facebook: [],
+      twitter: [],
+      email: [],
+      phone: []
+    }
+  }
 
-  return (
-    <h1>UPDATE ACTION form</h1>
-  )
+  async componentDidMount() {
+    const facebookFetch = await fetch('/api/v1/facebook_actions');
+    const facebookActions = await facebookFetch.json();
+    const facebook = facebookActions.results;
+
+    const twitterFetch = await fetch('/api/v1/twitter_actions');
+    const twitterActions = await twitterFetch.json();
+    const twitter = twitterActions.results;
+
+    const emailFetch = await fetch('/api/v1/email_actions');
+    const emailActions = await emailFetch.json();
+    const email = emailActions.results;
+
+    const phoneFetch = await fetch('/api/v1/phone_actions');
+    const phoneActions = await phoneFetch.json();
+    const phone = phoneActions.results;
+
+    await this.setState({ facebook, twitter, email, phone });
+  };
+
+  handleChange = () => {
+    const actionType = this.updateActionTypes.value;
+    
+    this.setState({ actionType });
+  }
+
+  render() {
+    const actions = this.state[this.state.actionType].map((action, i) => {
+      return (
+        <li key={`li-${i}`} className='action'>{`ACTION ${i}: ${action.title}`}</li>
+      )
+    });
+
+    return (
+      <div className='UpdateAction'>
+        <h1>Enable and Disable <span>{this.state.actionType.toUpperCase()}</span> Actions</h1>
+        <label htmlFor='action-types'>Select Action Type:
+          <select onChange={() => this.handleChange()} ref={(elem) => {this.updateActionTypes = elem}} name='action-types' id='action-types'>
+            <option value='facebook'>Facebook</option>
+            <option value='twitter'>Twitter</option>
+            <option value='email'>Email</option>
+            <option value='phone'>Phone</option>
+          </select>
+        </label>
+        <div className='actions-container'>
+          <ul className='actions'>
+            {actions}
+          </ul>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default UpdateAction;

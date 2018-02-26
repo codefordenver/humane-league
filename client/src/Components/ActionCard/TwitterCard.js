abfa7c3141f739ca8037d40754b87407dbafc8d6
katiescruggs <katie.e.scruggs@gmail.com>
2018-02-25 17:44:59 -0700

    Twitter body shows on click

--- client/src/Components/ActionCard/TwitterCard.js
+++ client/src/Components/ActionCard/TwitterCard.js
@@ -2,23 +2,47 @@ import React from 'react';
 import twitterLogo from '../../assets/twitter.png';
 import logAction from '../../utils/logAction';
 
-const TwitterCard = ({ action, user }) => {
-  const { title, description, target } = action;
+class TwitterCard extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      showMore: null
+    }
+  }
 
-  return (
-    <div className="ActionCard twitter-card">
-      <div className="action-logo-holder">
-        <img className="action-logo" src={twitterLogo} alt="Twitter Symbol"/>
+  fetchBodies = async (contentsTable, action) => {
+    const actionBodiesFetch = await fetch(`/api/v1/${contentsTable}`);
+    const actionBodies = await actionBodiesFetch.json();
+
+    const bodies = actionBodies.results.filter(body => body.action_id === this.props.action.id);
+    const body = bodies[Math.floor(Math.random() * bodies.length)];
+
+    console.log(body);
+
+    this.setState({ showMore: <textarea value={body.content}></textarea> }) ;
+  }
+
+  //logAction('twitter_actions', user, action)
+  render() {
+    const { title, description, target } = this.props.action;
+    return (
+      <div className="ActionCard twitter-card">
+        <div className="action-logo-holder">
+          <img className="action-logo" src={twitterLogo} alt="Twitter Symbol"/>
+        </div>
+        <div className="action-card-main">
+          <h3>{title}</h3>
+          <p>{description}</p>
+
+          {this.state.showMore}
+
+          <button onClick={() => this.fetchBodies('twitter_contents', this.props.action)}>CLICK TO TWEET</button>
+          
+         
+        </div>
       </div>
-      <div className="action-card-main">
-        <h3>{title}</h3>
-        <p>{description}</p>
-        <a target="_blank" href={target}>
-          <button onClick={() => logAction('twitter_actions', user, action)}>CLICK TO RE-TWEET</button>
-        </a> 
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default TwitterCard;

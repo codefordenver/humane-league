534255c0702a6279dd6a7161aeb61f4057234697
julie hawkins <julabi@gmail.com>
2018-02-27 11:24:45 -0700

    creates test snapshots for action cards:

---
+++ client/src/Components/ActionCard/ActionCard.test.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+
+import TwitterCard from './TwitterCard';
+import FacebookCard from './FacebookCard';
+import EmailCard from './EmailCard';
+import PhoneCard from './PhoneCard';
+
+describe('TwitterCard component tests', () => {
+  let renderedTwitterCard;
+  let mockAction;
+  let mockUser;
+  beforeEach(() => {
+    mockAction = {};
+    mockUser = {};
+    renderedTwitterCard = shallow( <TwitterCard action={mockAction} user={mockUser}/> );
+  })
+  it('renders without crashing', () => {
+    expect(renderedTwitterCard).toBeDefined();
+  });
+  it('matches SnapShot', () => {
+    expect(renderedTwitterCard).toMatchSnapshot();
+  })
+  
+})
+
+describe('FacebookCard component tests', () => {
+  let renderedFacebookCard;
+  let mockAction;
+  let mockUser;
+  beforeEach(() => {
+    mockAction = {};
+    mockUser = {};
+    renderedFacebookCard = shallow( <FacebookCard action={mockAction} user={mockUser}/> );
+  })
+  it('renders without crashing', () => {
+    expect(renderedFacebookCard).toBeDefined();
+  });
+  it('matches SnapShot', () => {
+    expect(renderedFacebookCard).toMatchSnapshot();
+  })
+  
+})
+
+describe('EmailCard component tests', () => {
+  let renderedEmailCard;
+  let mockAction;
+  let mockUser;
+  beforeEach(() => {
+    mockAction = {};
+    mockUser = {};
+    renderedEmailCard = shallow( <EmailCard action={mockAction} user={mockUser}/> );
+  })
+  it('renders without crashing', () => {
+    expect(renderedEmailCard).toBeDefined();
+  });
+  it('matches SnapShot', () => {
+    expect(renderedEmailCard).toMatchSnapshot();
+  })
+  
+})
+
+describe('PhoneCard component tests', () => {
+  let renderedPhoneCard;
+  let mockAction;
+  let mockUser;
+  beforeEach(() => {
+    mockAction = {};
+    mockUser = {};
+    renderedPhoneCard = shallow( <PhoneCard action={mockAction} user={mockUser}/> );
+  })
+  it('renders without crashing', () => {
+    expect(renderedPhoneCard).toBeDefined();
+  });
+  it('matches SnapShot', () => {
+    expect(renderedPhoneCard).toMatchSnapshot();
+  })
+  
+})

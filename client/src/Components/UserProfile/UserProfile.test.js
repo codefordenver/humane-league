119bd12f005b5bdc1bc9bfab7d7cd1ba71838648
Thomas Laird <t6r6l5@gmail.com>
2018-02-27 20:43:45 -0700

    fix beforeeach information to pass tests

--- client/src/Components/UserProfile/UserProfile.test.js
+++ client/src/Components/UserProfile/UserProfile.test.js
@@ -3,37 +3,33 @@ import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import { UserProfile } from './UserProfile';
 
-window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
-  json: () => 
-    Promise.resolve(
-      [{ user_id: 1 }]
-    )   
-}));
-describe.skip('UserProfile component tests', () => {
+describe('UserProfile component tests', () => {
   let renderedUserProfile;
   let defaultState;
   let mockUser; 
-  let userPrefereces;
+  let userPreferences;
   beforeEach(() => {
-    userPrefereces =  { twitter_actions: false, facebook_actions: false, email_actions: false, phone_actions: false };
-    mockUser = { id: 1, name: 'Julie Hawkins', email: 'Julabi@gmail.com', ...userPrefereces };
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      json: () => Promise.resolve({ results: []})
+    }));
+    userPreferences =  { twitter_actions: false, facebook_actions: false, email_actions: false, phone_actions: false };
+    mockUser = { id: 1, name: 'Julie Hawkins', email: 'Julabi@gmail.com', ...userPreferences };
     renderedUserProfile = shallow( <UserProfile user={mockUser}/> );
-    defaultState ={
-      name: props.user.name,
-      email: props.user.email,
-      actionCount: true,
-      twitter_actions: true,
-      facebook_actions: true,
-      email_actions: true,
-      phone_actions: true
-    }
-
-  })
+    defaultState = {
+      name: 'Julie Hawkins',
+      email: 'Julabi@gmail.com',
+      actionCount: 0,
+      twitter_actions: false,
+      facebook_actions: false,
+      email_actions: false,
+      phone_actions: false
+    };
+  });
   it('renders without crashing', () => {
     expect(renderedUserProfile).toBeDefined();
     expect(renderedUserProfile.state()).toEqual(defaultState);
   });
   it('matches SnapShot', () => {
     expect(renderedUserProfile).toMatchSnapshot();
-  })
-})
+  });
+});

6cecd12db6e4b92a00529a267c7d4b3505119d20
julie hawkins <julabi@gmail.com>
2018-03-06 13:55:25 -0700

    removes addcontent file and route

--- client/src/Components/AdminDashboard/AdminDashboard.js
+++ client/src/Components/AdminDashboard/AdminDashboard.js
@@ -7,7 +7,6 @@ import PropTypes from 'prop-types';
 import Dashboard from './Dashboard';
 import CreateNewAction from '../AdminForms/CreateNewAction';
 import UpdateAction from '../AdminForms/UpdateAction';
-import AddContent from '../AdminForms/AddContent';
 import ActionLog from '../AdminForms/ActionLog';
 import UserFeedback from '../AdminForms/UserFeedback';
 import AdminActions from '../AdminActions/AdminActions';
@@ -27,7 +26,6 @@ class AdminDashboard extends Component {
           <Route exact path='/admin' component={Dashboard} />
           <Route path='/admin/create' component={CreateNewAction} />
           <Route path='/admin/update' component={UpdateAction} />
-          <Route path='/admin/addcontent' component={AddContent} />
           <Route path='/admin/log' component={ActionLog} />
           <Route path='/admin/feedback' component={UserFeedback} />
           <Route path='/admin/actions' component={AdminActions} />
